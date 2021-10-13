4cd132e928e3382bbc10142babf3ef7db5195227
Антон <sierra_93@mail.ru>
2021-10-14 00:44:17 +0300

    Добавил каталог франшиз.

--- src/app/modules/catalog-franchise/catalog-franchise.component.ts
+++ src/app/modules/catalog-franchise/catalog-franchise.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Title } from "@angular/platform-browser";
 import { API_URL } from "src/app/core/core-urls/api-url";
+import { FilterInput } from "src/app/models/franchise/input/filter-franchise-input";
 import { FranchiseInput } from "src/app/models/franchise/input/franchise-input";
+import { PaginationInput } from "src/app/models/pagination/input/pagination-input";
 import { CommonDataService } from "src/app/services/common-data.service";
 
 @Component({
@@ -18,8 +20,8 @@ export class CatalogFranchiseModule implements OnInit {
     aCities: any[] = [];
     aBusinessCategories: any[] = [];
     aViewBusiness: any[] = [];
-    minPrice: number = 0;
-    maxPrice: number = 0;
+    minPrice!: number;
+    maxPrice!: number;
     view: string = "";
     city: string = "";
     category: string = "";
@@ -29,14 +31,21 @@ export class CatalogFranchiseModule implements OnInit {
     aFranchises: any[] = [];
     selectedSort: any;
     aSortPrices: any[] = [];
+    filterMinPrice!: number;
+    filterMaxPrice!: number;
+    countTotalPage!: number;
+    countFranchises!: number;
 
     constructor(private http: HttpClient, private commonService: CommonDataService, private titleService: Title) {
+        // TODO: Переделать на хранение на бэке.
         this.aSortPrices = [
             {
-                key: "По убыванию цены"
+                name: "По убыванию цены",
+                value: "Desc"
             },
             {
-                key: "По возрастанию цены"
+                name: "По возрастанию цены",
+                value: "Asc"
             }
         ];
     };
@@ -49,6 +58,7 @@ export class CatalogFranchiseModule implements OnInit {
         await this.loadCitiesFranchisesListAsync();
         await this.loadCategoriesFranchisesListAsync();
         await this.loadViewBusinessFranchisesListAsync();
+        await this.loadPaginationInitAsync();
     };   
 
     /**
@@ -197,4 +207,97 @@ export class CatalogFranchiseModule implements OnInit {
             throw new Error(e);
         }
     };    
+
+    public async onPaginationChangeAsync(event: any) {
+        let paginationData = new PaginationInput();
+        paginationData.PageNumber = event.page + 1;
+        paginationData.CountRows = event.rows;
+
+        try {
+            await this.http.post(API_URL.apiUrl.concat("/pagination/catalog-franchise"), paginationData)
+            .subscribe({
+                next: (response: any) => {
+                    console.log("get data pagination", response);
+                    this.countFranchises = response.countAll;
+                    this.aFranchises = response.results;
+                    // this.router.navigate(['/auction'], {
+                    //     queryParams: {
+                    //         page: paginationData.PageNumber,
+                    //         rows: paginationData.CountRows
+                    //     }
+                    // });
+                },
+
+                error: (err) => {
+                    this.commonService.routeToStart(err);
+                    throw new Error(err);
+                }
+            });
+        }
+
+        catch (e: any) {
+            throw new Error(e);
+        }
+    };
+
+    private async loadPaginationInitAsync() {
+        let paginationData = new PaginationInput();
+
+        // TODO: доработать на динамическое получение из роута или как-нибудь еще, чтобы помнить, что выбирал пользователь.
+        paginationData.PageNumber = 1;
+
+        try {
+            await this.http.post(API_URL.apiUrl.concat("/pagination/init-catalog-franchise"), paginationData)
+            .subscribe({
+                next: (response: any) => {
+                    console.log("pagination init", response);
+                    this.countFranchises = response.countAll;
+                    this.aFranchises = response.results;
+                },
+
+                error: (err) => {
+                    this.commonService.routeToStart(err);
+                    throw new Error(err);
+                }
+            });
+        }
+
+        catch (e: any) {
+            throw new Error(e);
+        }
+    };
+
+    public async onChangeSortPrice() {
+        console.log("onChangeSortPrice", this.selectedSort);
+    };
+
+    public async FilterFranchisesAsync() {
+        try {
+            let filterInput = new FilterInput();
+            filterInput.TypeSortPrice = this.selectedSort.value;
+            filterInput.ProfitMinPrice = this.filterMinPrice.toString();
+            filterInput.ProfitMaxPrice = this.filterMaxPrice.toString();
+
+            await this.http.post(API_URL.apiUrl.concat("/franchise/filter-franchises"), filterInput)
+            .subscribe({
+                next: (response: any) => {
+                    console.log("Франшизы после фильтрации:", response);                    
+                    this.aFranchises = response;
+                },
+
+                error: (err) => {
+                    this.commonService.routeToStart(err);
+                    throw new Error(err);
+                }
+            });
+        }
+
+        catch (e: any) {
+            throw new Error(e);
+        }
+    };
+
+    public async onClearFilters() {
+        await this.GetFranchisesListAsync();
+    };
 }

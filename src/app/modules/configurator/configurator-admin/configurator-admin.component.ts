5bf8deaf6433c6af013c622920b7b45e9156568c
Антон <sierra_93@mail.ru>
2022-01-30 15:23:23 +0300

    Добавил вывод меню конфигуратора.

--- src/app/modules/configurator/configurator-admin/configurator-admin.component.ts
+++ src/app/modules/configurator/configurator-admin/configurator-admin.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { API_URL } from "src/app/core/core-urls/api-url";
 
 @Component({
     selector: "configurator-admin",
@@ -11,15 +12,45 @@ import { Component, OnInit } from "@angular/core";
  * Класс модуля конфигуратора (панель).
  */
 export class ConfiguratorAdminModule implements OnInit {
-    items: any[] = [
-        { label: 'Home', icon: 'pi pi-fw pi-home', header: 'Header 1' },
-        { label: 'Calendar', icon: 'pi pi-fw pi-calendar', header: 'Header 2' },
-        { label: 'Edit', icon: 'pi pi-fw pi-pencil', header: 'Header 3' },
-        { label: 'Documentation', icon: 'pi pi-fw pi-file', header: 'Header 4' },
-        { label: 'Settings', icon: 'pi pi-fw pi-cog', header: 'Header 5' }
-    ];;
+
+    aMenuList: any[] = [];
+
+    constructor(private http: HttpClient) {
+
+    };
+    // items: any[] = [
+    //     { label: 'Home', icon: 'pi pi-fw pi-home', header: 'Header 1' },
+    //     { label: 'Calendar', icon: 'pi pi-fw pi-calendar', header: 'Header 2' },
+    //     { label: 'Edit', icon: 'pi pi-fw pi-pencil', header: 'Header 3' },
+    //     { label: 'Documentation', icon: 'pi pi-fw pi-file', header: 'Header 4' },
+    //     { label: 'Settings', icon: 'pi pi-fw pi-cog', header: 'Header 5' }
+    // ];
 
     public async ngOnInit() {
+        await this.loadMenuItemsAsync();
+    };
 
+    /**
+     * Функция получит список меню конфигуратора.
+     * @returns - Список элементов меню.
+     */
+    private async loadMenuItemsAsync() {
+        try {                                 
+            await this.http.get(API_URL.apiUrl.concat("/configurator/menu-items"))
+                .subscribe({
+                    next: (response: any) => {                        
+                        console.log("menu list:", response);
+                        this.aMenuList = response;
+                    },
+
+                    error: (err) => {
+                        throw new Error(err);
+                    }
+                });
+        }
+
+        catch (e: any) {
+            throw new Error(e);
+        }
     };
 }

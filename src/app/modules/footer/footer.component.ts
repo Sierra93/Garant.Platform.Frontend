3be592d8cbdc77ce13c09bdd65cc956ea69aad84
Denis <den.shklyarskij@bk.ru>
2022-03-04 17:50:19 +0300

    Удалил вызов метода redirectCreateAd() в хуке ngOnInit

--- src/app/modules/footer/footer.component.ts
+++ src/app/modules/footer/footer.component.ts
@@ -28,7 +28,6 @@ export class FooterModule implements OnInit {
 
   public async ngOnInit() {
     await this.initFooter();
-    this.redirectCreateAd();
   }
 
   @HostListener('window:resize', ['$event'])

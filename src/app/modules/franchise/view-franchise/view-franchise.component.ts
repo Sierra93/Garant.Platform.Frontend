eb7016371c230376bbc5d48080700f2f3052c33c
Denis <den.shklyarskij@bk.ru>
2022-03-12 22:48:01 +0300

    Save clean code

--- src/app/modules/franchise/view-franchise/view-franchise.component.ts
+++ src/app/modules/franchise/view-franchise/view-franchise.component.ts
@@ -225,6 +225,7 @@ export class ViewFranchiseModule implements OnInit {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
   }
+  
   /**
    * Функция добавит файл презентации.
    */

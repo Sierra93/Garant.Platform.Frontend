ba5614218824107730410cf223921e4cec8fd29a
Denis <den.shklyarskij@bk.ru>
2022-03-26 20:15:24 +0300

    Вернул удаленный компонент гарант в папку компонентс и вернул его в шаблоны

--- src/app/components/garant/garant.component.ts
+++ src/app/components/garant/garant.component.ts
@@ -1,51 +1,15 @@
-import { Component, DoCheck, HostListener, OnInit } from '@angular/core';
-import { CommonDataService } from 'src/app/services/common/common-data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-garant',
   templateUrl: './garant.component.html',
-  styleUrls: ['./garant.component.scss'],
+  styleUrls: ['./garant.component.scss']
 })
-export class GarantComponent implements OnInit, DoCheck {
-  oSuggestion: any = {};
-  isHideBusinessWithGarant: boolean = true;
-  isXxl!: boolean;
-  browserScreenWidth!: number;
+export class GarantComponent implements OnInit {
 
-  constructor(private commonService: CommonDataService) {}
+  constructor() { }
 
-  public async ngOnInit(): Promise<void> {
-    this.isXxl = false;
-    this.browserScreenWidth = window.screen.width;
-
-    await this.loadSingleSuggestionAsync();
+  ngOnInit(): void {
   }
 
-  public ngDoCheck(): void {
-    this.defineResize();
-  }
-
-  /**
-   * Функция получит одно предложение с флагом IsSingle.
-   * @returns данные предложения.
-   */
-  private async loadSingleSuggestionAsync() {
-    try {
-      await this.commonService.loadSingleSuggestionAsync().then((data: any) => {
-        this.oSuggestion = data;
-      });
-    } catch (e: any) {
-      throw new Error(e);
-    }
-  }
-  
-  @HostListener('window:resize', ['$event'])
-  private defineResize() {
-    this.browserScreenWidth = window.screen.width;
-    if (this.browserScreenWidth > 1400) {
-      this.isXxl = true;
-    } else {
-      this.isXxl = false;
-    }
-  }
 }
